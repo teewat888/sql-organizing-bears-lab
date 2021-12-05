03aa30c06642f4d72518148e4ba7d34beb18f857
teerawat prarom <teewat@yahoo.com>
2021-12-06 10:13:19 +1100

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'Bad', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 15, 'M', 'Brown', 'Bad', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 51, 'F', 'RED', 'Goodd', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergent Brown', 25, 'M', 'White', 'Bad', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 35, 'F', 'Brown', 'Bad', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 45, 'M', 'Brown', 'Bad', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 7, 'F', 'Brown', 'Bad', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 12, 'M', 'Brown', 'Bad', 1);
